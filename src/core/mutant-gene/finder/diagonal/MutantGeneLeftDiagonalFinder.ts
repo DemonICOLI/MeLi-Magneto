d26b1d63b428deb4534d50f81339c4cb413da47a
Javier <jcamilo.quinones@gmail.com>
2021-02-25 12:29:02 -0500

    :sparkles: MELI_0003: Validacion de gen Mutante en Diagonales

---
+++ src/core/mutant-gene/finder/diagonal/MutantGeneLeftDiagonalFinder.ts
@@ -0,0 +1,29 @@
+import {MutantGeneFinder} from "../MutantGeneFinder";
+import {Genome} from "../../../../model/mutant-gene/Genome";
+import {GeneSequence} from "../../../../model/mutant-gene/GeneSequence";
+import {CONSTANTS} from "../../../../utils/Constants";
+
+export class MutantGeneLeftDiagonalFinder extends MutantGeneFinder {
+
+    extractGeneSequences(genome: Genome): GeneSequence[] {
+        let geneSequences : GeneSequence[] = [];
+        const genomeLength = genome.length;
+        for(let i = 0; i <= (genomeLength * 2) - 1; i++){
+            const leftDiagonal:GeneSequence = [];
+            for(let y = genomeLength -1; y >= 0; y--){
+                const x = i - (genomeLength - y);
+                if(x >= 0 && x < genomeLength) {
+                    leftDiagonal.push(genome[y][x]);
+                }
+            }
+            if(leftDiagonal.length > 0){
+                geneSequences.push(leftDiagonal);
+            }
+        }
+        geneSequences = geneSequences.filter((geneSequence) => {
+            return geneSequence.length >= CONSTANTS.NUMBER_OF_CONSECUTIVE_GENES_TO_BE_MUTANT;
+        })
+        return geneSequences;
+    }
+
+}
